be2643fb8875b8fc7c0a230f6119d3b6ac169130
bingbingbz <120102692+bingbingbz@users.noreply.github.com>
2023-03-20 16:26:50 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYINDEX -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
+cat /etc/caddy/Caddyfile
 wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$SSENCYPT/$SSENCYPT/g" >/xray.json
 
 # storefiles
